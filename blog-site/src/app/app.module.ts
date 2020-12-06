8f1e59e5aef0be1a5dfdc052aa48fbbc4fb42d06
Ray C Horn <308035+raychorn@users.noreply.github.com>
2020-12-05 17:39:17 -0700

    updates.

--- blog-site/src/app/app.module.ts
+++ blog-site/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
 import { RightSidebarComponent } from './components/right-sidebar/right-sidebar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { RightSidebarComponent } from './components/right-sidebar/right-sidebar.
     HeaderComponent,
     FooterComponent,
     LeftSidebarComponent,
-    RightSidebarComponent
+    RightSidebarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
