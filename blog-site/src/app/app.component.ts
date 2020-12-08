3aae07b68d217e1519bfb1fee50d9ea84af93b56
Ray C Horn <308035+raychorn@users.noreply.github.com>
2020-12-08 08:22:57 -0700

    updates. adjustments for mobile devices.

--- blog-site/src/app/app.component.ts
+++ blog-site/src/app/app.component.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 import { CookieService } from './services/cookie.service';
 import { ScriptService } from './services/scripts.service';
 
-//import * as Module from 'src/assets/js/pyodide.js'; // raychorn.github.io/blog-site/src/assets/js/pyodide.js
-
-//import '!!file-loader?name=wasm/pyodide.asm.wasm!src/assets/js/pyodide.asm.wasm'; // raychorn.github.io/blog-site/src/assets/js/pyodide.asm.wasm
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,31 +12,18 @@ export class AppComponent {
 
   public now: Date = new Date();
 
-  public module: any;
-
-  private async instantiateWasm(url: string) {
-    // fetch the wasm file
-    const wasmFile = await fetch(url);
-
-    // convert it into a binary array
-    const buffer = await wasmFile.arrayBuffer();
-    const binary = new Uint8Array(buffer);
-
-    const moduleArgs = {
-      wasmBinary: binary,
-      onRuntimeInitialized: () => {
-        // TODO
-      }
-    };
-    // instantiate the module
-    //this.module = Module(moduleArgs);
-  }
-
   constructor(private cookieService: CookieService, private scriptService: ScriptService) {
-    console.log('Loading External Scripts');
+    //console.log('Loading External Scripts');
     //this.scriptService.load('pyodide');
     //this.instantiateWasm('wasm/pyodide.asm.wasm');
     //console.info('AppComponent :: this.module -> ' + this.module);
+  }
+
+  getOrientation(): string {
+    if (window.outerWidth > window.outerHeight) {
+      return 'landscape';
+    }
+    return 'portrait';
   }
 
   setWASMSupport(has_support: boolean): void {
@@ -70,6 +53,7 @@ export class AppComponent {
     var is_supported: boolean = this.detectWASM();
     console.info('WASM is_supported -> ' + is_supported);
     this.setWASMSupport(is_supported);
+    console.info('*** getOrientation() -> ' + this.getOrientation());
   }
 
 
